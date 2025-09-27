cbc36b69c7f72cc392f12c0ddc18e08fdeab1a91
AbdujalolovaE'zoza <karimovaezoza2006@gmail.com>
2025-09-27 23:49:43 +0500

    plan.txt

--- main.js
+++ main.js
@@ -3,3 +3,17 @@
 // let age=18;
 // let result2 = age > 18 ? "kotta": age===18 ? "teng": "kichik";
 // console.log(result2);
+
+const number=5;
+const result= number >0 ? "Positive" : "Negative";
+console.log(result);
+
+const score = 85;
+const grade =score >=90
+? "a"
+: score >=80
+? "b"
+: score >=70
+? "c"
+: "f"
+console.log (grade);
